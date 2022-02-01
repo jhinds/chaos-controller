3bd4cb12a52b0e52759953f4bdaa155cf8db0d1d
Philip Thompson <philip.thompson@datadoghq.com>
2022-02-01 14:48:18 -0500

    Explicitly use default rate limiter

--- controllers/disruption_controller.go
+++ controllers/disruption_controller.go
@@ -29,8 +29,10 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -1179,6 +1181,7 @@ func (r *DisruptionReconciler) SetupWithManager(mgr ctrl.Manager, kubeInformerFa
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&chaosv1beta1.Disruption{}).
 		Watches(&source.Informer{Informer: informer}, handler.EnqueueRequestsFromMapFunc(podToDisruption)).
+		WithOptions(controller.Options{RateLimiter: workqueue.DefaultControllerRateLimiter()}).
 		Complete(r)
 }
 
